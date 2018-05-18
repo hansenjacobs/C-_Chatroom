1dde8d10cc4af8300642feec282148399b158459
ckloskey <you@example.com>
2018-05-18 11:11:13 -0500

    Set up threads and loops

---
+++ Server/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Client
+    {
+        NetworkStream stream;
+        TcpClient client;
+        public string UserId;
+        public Queue<byte []> messageQueue;
+        public Client(NetworkStream Stream, TcpClient Client, Queue <byte []> messageQueue)
+        {
+            stream = Stream;
+            client = Client;
+            UserId = "495933b6-1762-47a1-b655-483510072e73";
+            this.messageQueue = messageQueue;
+        }
+        public void Send(byte[] message)
+        {
+            //byte[] message = Encoding.ASCII.GetBytes(Message);
+            stream.Write(message, 0, message.Count());
+        }
+        public string Receive()
+        {
+            while (true)
+            {
+            byte[] recievedMessage = new byte[256];
+            stream.Read(recievedMessage, 0, recievedMessage.Length);
+            string recievedMessageString = Encoding.ASCII.GetString(recievedMessage);
+                messageQueue.Enqueue(recievedMessage);
+            Console.WriteLine(recievedMessageString);
+            return recievedMessageString;
+            }
+        }
+
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+        public static Client client;
+        TcpListener server;
+        Queue<byte[]> messageQueue;
+        Dictionary <string, Client> clients;
+        
+        public Server()
+        {
+            server = new TcpListener(IPAddress.Parse("127.0.0.1"), 9999);
+            server.Start();
+            messageQueue = new Queue<byte[]>();
+            clients = new Dictionary<string, Client>();
+        }
+        
+        public void Run()
+        {
+            Parallel.Invoke(
+            ()=>
+            {
+                AcceptClient();
+            },
+            ()=>
+            {
+                DeliverQueueMessages();
+            }
+            );
+            
+        }
+        
+        private void AcceptClient()
+        {
+            while (true)
+            { 
+                    //find better condition later 
+                TcpClient clientSocket = default(TcpClient);
+                clientSocket = server.AcceptTcpClient();
+                
+                Console.WriteLine("Connected");
+                NetworkStream stream = clientSocket.GetStream();
+                clients.Add(clientSocket.Client.RemoteEndPoint.ToString(), new Client(stream, clientSocket, messageQueue));
+                Parallel.Invoke(()=> 
+                {
+                    clients[clientSocket.Client.RemoteEndPoint.ToString()].Receive();
+                }
+                    );
+            }
+        }
+
+        private void DeliverQueueMessages()
+        {
+            while (true)
+            {
+                if (CheckQueue())
+                {
+                    Respond(messageQueue.Dequeue());
+                }
+            }
+        }
+        private void Respond(byte[] body)
+        {
+             client.Send(body);
+        }
+
+        private bool CheckQueue()
+        {
+            if (messageQueue.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }  
+        }
+
+        //private void Chat(Server.Respond, Client.Send)
+    }
+}
