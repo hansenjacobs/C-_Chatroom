0a15c4a420ec5eba7e90e9959f60ba3fd43bec2e
Jacob Hansen <hansenjacobs@gmail.com>
2018-05-21 13:04:57 -0500

    changed Server Message sender variable to auto-property

--- Server/Message.cs
+++ Server/Message.cs
@@ -8,17 +8,17 @@ namespace Server
 {
     class Message
     {
-        public Client sender;
         public Message(Client Sender, byte[] encodedMessage)
         {
-            sender = Sender;
+            this.Sender = Sender;
             EncodedMessage = encodedMessage;
-            Body = Encoding.ASCII.GetString(encodedMessage);
+            Body = Encoding.ASCII.GetString(encodedMessage).Trim();
             ReceivedDateTime = DateTime.Now;
         }
 
         public string Body { get; set; }
         public byte[] EncodedMessage { get; set; }
         public DateTime ReceivedDateTime { get; private set; }
+        public Client Sender {get; set;}
     }
 }
