cc084b05f309526f6cb3fa3223c8f9cb0221ba40
ckloskey <you@example.com>
2018-05-21 11:44:30 -0500

    ILogger interface and TextLogger Class added

---
+++ Server/TextLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class TextLogger : ILogger
+    {
+        string filePath = Environment.CurrentDirectory;
+
+        public void DoLog(string message)
+        {
+            StreamWriter textFile = new StreamWriter(filePath);
+            textFile.WriteLine(message);
+        }
+    }
+
+    public interface ILogger
+    {
+        void DoLog(string message);
+    }
+}
