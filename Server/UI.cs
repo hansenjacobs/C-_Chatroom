48e722d8affb5a6c5e9ce71a02ecc90ed4391ba6
Jacob Hansen <hansenjacobs@gmail.com>
2018-05-21 08:52:21 -0500

    VS normalized line endings

---
+++ Server/UI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class UI
+    {
+        public static bool ServerRunning { get; set; } = true;
+
+        public static string GetUserInput()
+        {
+            return Console.ReadLine();
+        }
+
+        public static void AwaitCommand()
+        {
+
+            while (ServerRunning)
+            {
+                string command = GetUserInput();
+
+                switch (command)
+                {
+                    case "quit":
+                        ServerRunning = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invlid command.");
+                        break;
+                }
+            }
+        }
+    }
+}
